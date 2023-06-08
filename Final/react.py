27c22ae8f4f3aa694333bc20ebc84dd5ef95e011
Vedant Rajendra Shetye <97832138+NOOBPOOK@users.noreply.github.com>
2023-06-08 20:27:18 +0530

    Update react.py

--- Final/react.py
+++ Final/react.py
@@ -1,27 +1,44 @@
 import llama_index
 from llama_index import VectorStoreIndex, SimpleDirectoryReader,ServiceContext
 from langchain.chat_models import ChatOpenAI
+import codecs
 
-llm1 = ChatOpenAI(temperature=0.1,model='gpt-3.5-turbo')
-llm2 = ChatOpenAI(temperature=0.2,model='gpt-3.5-turbo-0301')
-llm3 = ChatOpenAI(temperature=0.1,model='gpt-3.5-turbo')
-llm4 = ChatOpenAI(temperature=0.3,model='gpt-3.5-turbo-0301')
-llm5 = ChatOpenAI(temperature=0.5,model='gpt-3.5-turbo')
-llm6 = ChatOpenAI(temperature=0.4,model='gpt-3.5-turbo-0301')
+#TO DIVIDE THE INPUT FILE INTO SIX DIFFERENT PARTS
+gh = []
+with codecs.open("bigdata.txt", 'r', encoding='utf-8',errors='ignore') as fdata:
+    gh = fdata.readlines()
 
-chp1 = SimpleDirectoryReader(input_dir="data/chp1.txt").load_data()
-chp2 = SimpleDirectoryReader(input_dir="data/chp2.txt").load_data()
-chp3 = SimpleDirectoryReader(input_dir="data/chp3.txt").load_data()
-chp4 = SimpleDirectoryReader(input_dir="data/chp4.txt").load_data()
-chp5 = SimpleDirectoryReader(input_dir="data/chp5.txt").load_data()
-chp6 = SimpleDirectoryReader(input_dir="data/chp6.txt").load_data()
+li = ["CHAPTER I", "CHAPTER II", "CHAPTER III", "CHAPTER IV", "CHAPTER V", "CHAPTER VI","123456789"]
+st = 0
+ok = open('file.txt','w')
+
+for i in gh:
+    if li[st] in i:
+        ok.close()
+        ok = open(f"{li[st]}"+".txt","w")
+        st += 1
+    else:
+        try:
+            ok.write(i)
+        except:
+            pass
+ok.close()
+
+
+chp1 = SimpleDirectoryReader(input_dir="CHAPTER I.txt").load_data()
+chp2 = SimpleDirectoryReader(input_dir="CHAPTER II.txt").load_data()
+chp3 = SimpleDirectoryReader(input_dir="CHAPTER III.txt").load_data()
+chp4 = SimpleDirectoryReader(input_dir="CHAPTER IV.txt").load_data()
+chp5 = SimpleDirectoryReader(input_dir="CHAPTER V.txt").load_data()
+chp6 = SimpleDirectoryReader(input_dir="CHAPTER VI.txt").load_data()
 
 arr = [chp1,chp2,chp3,chp4,chp5,chp6]
-arlm = [llm1,llm2,llm3,llm4,llm5,llm6]
+arlm = ["0.2","0.1","0.3","0.4","0.5","0.1"]
 
 choice = int(input("Select which chapter to choose from ...1 to 6  >>"))-1
 
-service = ServiceContext.from_defaults(llm = arlm[choice],)
+llm = ChatOpenAI(temperature=arlm[choice],model='gpt-3.5-turbo')
+service = ServiceContext.from_defaults(llm = llm)
 index = VectorStoreIndex.from_documents(arr[choice])
 
 chat_eng = index.as_chat_engine(service_context = service, chat_mode='react', verbose=True)
